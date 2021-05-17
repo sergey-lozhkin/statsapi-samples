3fff14282364437406c4064b32bd969f681e25a2
Sergey Lozhkin <sergey.lozhkin@brightpattern.com>
2021-05-17 14:50:25 -0700

    more correct ws upgrade

--- 9-ws.sh
+++ 9-ws.sh
@@ -8,15 +8,21 @@ ${CURL} --include --no-buffer \
     --header "Origin: http://sergey-lozhkin:10070" \
     --header "Connection: Upgrade" \
     --header "Upgrade: websocket" \
-    --header "Sec-WebSocket-Key: SGVsbG8sIHdvcmxkIQ==" \
+    --header "Sec-WebSocket-Key: QKXwwg830iF9oZT46q4zKQ==" \
     --header "Sec-WebSocket-Version: 13" \
 
-    # --data-urlencode "username=helper2" \
-    # --data-urlencode "token=relaytoken2" \
+exit 0
 
-    # --header 'Content-Type: application/json;charset=UTF-8' \
-    # --data '{ "raw": { "json": "data" } }'
+    --header "Sec-WebSocket-Extensions: permessage-deflate; client_max_window_bits" \
+    --header 'Content-Type: application/json;charset=UTF-8' \
+    --data '{ "raw": { "json": "data" } }'
+    --data-urlencode "username=helper" \
+    --data-urlencode "token=relaytoken" \
 
-    # --header "Origin: http://sergey-lozhkin:10070" \
-    # /pipe/relay\?username=helper\&token=relaytoken
-    # "http://sergey-lozhkin:10070/pipe/relay" \
+    --header "Origin: http://sergey-lozhkin:10070" \
+    /pipe/relay\?username=helper\&token=relaytoken
+    "http://sergey-lozhkin:10070/pipe/relay" \
+
+    "http://sergey-lozhkin:10070/pipe/relay" \
+
+    --user-agent "" \
